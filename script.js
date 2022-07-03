2602e059887930962edd0f8909c39727a0359be1
Discordmodsbers <94165954+Discordmodsbers@users.noreply.github.com>
2022-07-03 01:03:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-document.addEventListener('mouseout', e => {
-    if (!e.toElement && !e.relatedTarget) {
-        document.querySelector('.exit-intent-popup').classList.add('visible'); 
-    }
-});
+// Wrap the setTimeout into an if statement
+if (!CookieService.getCookie('exitIntentShown')) {
+    setTimeout(() => {
+        document.addEventListener('mouseout', mouseEvent);
+        document.addEventListener('keydown', exit);
+    }, 10_000);
+}
+
 const mouseEvent = e => {
-    if (!e.toElement && !e.relatedTarget) {
+    const shouldShowExitIntent = 
+        !e.toElement && 
+        !e.relatedTarget &&
+        e.clientY < 10;
+
+    if (shouldShowExitIntent) {
         document.removeEventListener('mouseout', mouseEvent);
-        
         document.querySelector('.exit-intent-popup').classList.add('visible');
+        
+        // Set the cookie when the popup is shown to the user
+        CookieService.setCookie('exitIntentShown', true, 30);
     }
 };
-
-document.addEventListener('mouseout', mouseEvent);
