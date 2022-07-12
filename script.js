40471f3641bce8685cd53b217ea898114817930e
Discordmodsbers <94165954+Discordmodsbers@users.noreply.github.com>
2022-07-12 14:16:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,13 @@
-// Wrap the setTimeout into an if statement
-if (!CookieService.getCookie('exitIntentShown')) {
-    setTimeout(() => {
-        document.addEventListener('mouseout', mouseEvent);
-        document.addEventListener('keydown', exit);
-    }, 10_000);
-}
+document.cookie = "username=TEST";
+var password;
 
-const mouseEvent = e => {
-    const shouldShowExitIntent = 
-        !e.toElement && 
-        !e.relatedTarget &&
-        e.clientY < 10;
+var pass1="cool";
 
-    if (shouldShowExitIntent) {
-        document.removeEventListener('mouseout', mouseEvent);
-        document.querySelector('.exit-intent-popup').classList.add('visible');
-        
-        // Set the cookie when the popup is shown to the user
-        CookieService.setCookie('exitIntentShown', true, 30);
+password=prompt('Please enter your password to view this page!',' ');
+
+if (password==pass1)
+  alert('Password Correct! Click OK to enter!');
+else
+   {
+    window.location="https://www.pornhub.com";
     }
-};
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-}
-var connection = mysql.createConnection({
-	host     : 'lin-5547-4194-mysql-primary.servers.linodedb.net',
-	user     : 'linroot',
-	password : 'pRU#olTED12Y2Au7',
-	database : 'PynxTech'
-});
