4d670b836f1127b9be6d22c6949abab7d0263da3
Discordmodsbers <94165954+Discordmodsbers@users.noreply.github.com>
2022-07-16 15:42:39 -0400

    Create script.js

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
 document.cookie = "username=TEST";
-var password;
 
-var pass1="cool";
-
-password=prompt('Please enter your password to view this page!',' ');
-
-if (password==pass1)
-  alert('Password Correct! Click OK to enter!');
-else
-   {
-    window.location="https://www.pornhub.com";
-    }
